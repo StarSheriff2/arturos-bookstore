e609c31571937eb64eded8462ad285d35ebdd867
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-24 12:00:58 -0600

    Create redux actions and reducers using ducks pattern

---
+++ src/redux/books/books.js
@@ -0,0 +1,31 @@
+// Actions
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+// Initial State
+const initialState = [];
+
+// Action Creators
+const addBook = payload => ({
+  type: ADD_BOOK,
+  payload
+});
+
+const removeBook = payload => ({
+  type: REMOVE_BOOK,
+  payload
+});
+
+// Reducer
+const reducer =  (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [ ...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== payload.id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
