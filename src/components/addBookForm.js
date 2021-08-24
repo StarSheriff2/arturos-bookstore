f8fe32280e6a786d5c3b1372dbadd857196d1ecb
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-24 00:49:01 -0600

    add Add Book Form

--- src/components/addBookForm.js
+++ src/components/addBookForm.js
@@ -1,6 +1,15 @@
 const AddBookForm = () => (
   <>
-    <form action="#" className="add-book-form">Form</form>
+    <form action="#" className="add-book-form">
+      <input type="text" placeholder="Book title"/>
+      <select name="book-category" id="book-category">
+        <option value="" disabled selected hidden>Category</option>
+        <option value="action">action</option>
+        <option value="science fiction">science fiction</option>
+        <option value="economy">economy</option>
+      </select>
+      <button type="submit">Add book</button>
+    </form>
   </>
 );
 
