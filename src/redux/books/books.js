84414b43509b4cdc28ddad466ed7cd20831020b5
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-24 18:25:00 -0600

    Populate DOM in Bookstore page by consuming Redux state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,49 @@ const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 // Initial State
-const initialState = [];
+const initialState = {
+  bookList: [
+    {
+      titleInfo: {
+        id: 1,
+        title: 'The Hunger Games',
+        author: 'Suzanne Collins',
+        category: 'action',
+      },
+      progress: {
+        chapterNumber: 17,
+        chapterTitle: null,
+        completion: 0.65,
+      },
+    },
+    {
+      titleInfo: {
+        id: 2,
+        title: 'Dune',
+        author: 'Frank Herbert',
+        category: 'science fiction',
+      },
+      progress: {
+        chapterNumber: 3,
+        chapterTitle: 'A Lesson Learned',
+        completion: 0.8,
+      },
+    },
+    {
+      titleInfo: {
+        id: 3,
+        title: 'Capital in the 21st Century',
+        author: 'Suzanne Collins',
+        category: 'economy',
+      },
+      progress: {
+        chapterNumber: null,
+        chapterTitle: 'Introduction',
+        completion: 0,
+      },
+    },
+  ],
+};
 
 // Action Creators
 export const addBook = (payload) => ({
