55f2d81cba66944f658d112e2b8f93f3b26362d0
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-26 23:16:09 -0600

    Refresh page when clicking the logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,24 @@
 import Navbar from './navbar';
 import Login from './login';
 
-const Header = () => (
-  <header className="header">
-    <div className="header__sub-container">
-      <div className="header__sub-container__navbar-wrapper">
-        <h1 className="header__sub-container__logo">Bookstore CMS</h1>
-        <Navbar />
+const Header = () => {
+  const refreshPage = () => window.location.reload();
+
+  return (
+    <header className="header">
+      <div className="header__sub-container">
+        <div className="header__sub-container__navbar-wrapper">
+          <h1 className="header__sub-container__logo">
+            <button className="header__sub-container__logo__btn" type="button" onClick={refreshPage}>
+              Bookstore CMS
+            </button>
+          </h1>
+          <Navbar />
+        </div>
+        <Login />
       </div>
-      <Login />
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
